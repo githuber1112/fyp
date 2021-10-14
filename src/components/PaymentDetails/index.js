cbef26af2dc94ccff60ad07f1763adadee87478c
teoh <teohk-pm18@student.tarc.edu.my>
2021-10-14 22:31:57 +0800

    kj

--- src/components/PaymentDetails/index.js
+++ src/components/PaymentDetails/index.js
@@ -1,29 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import FormInput from './../forms/FormInput';
-import Button from './../forms/Button';
-import { CountryDropdown } from 'react-country-region-selector';
-import { apiInstance } from '../../utils';
-import { selectCartTotal, selectCartItemsCount, selectCartItems } from './../../redux/Cart/cart.selectors';
-import { saveOrderHistory } from './../../redux/Orders/orders.actions';
-import { clearCart } from './../../redux/Cart/cart.actions';
-import { createStructuredSelector } from 'reselect';
-import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import './styles.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCcAmex, faCcDiscover, faCcMastercard, faCcVisa } from '@fortawesome/free-brands-svg-icons';
+import React, { useState, useEffect } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import FormInput from "./../forms/FormInput";
+import Button from "./../forms/Button";
+import { CountryDropdown } from "react-country-region-selector";
+import { apiInstance } from "../../utils";
+import {
+  selectCartTotal,
+  selectCartItemsCount,
+  selectCartItems,
+} from "./../../redux/Cart/cart.selectors";
+import { saveOrderHistory } from "./../../redux/Orders/orders.actions";
+import { clearCart } from "./../../redux/Cart/cart.actions";
+import { createStructuredSelector } from "reselect";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import "./styles.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCcAmex,
+  faCcDiscover,
+  faCcMastercard,
+  faCcVisa,
+} from "@fortawesome/free-brands-svg-icons";
 
 const initialAddressState = {
-  line1: '',
-  line2: '',
-  city: '',
-  state: '',
-  postal_code: '',
-  country: '',
+  line1: "",
+  line2: "",
+  city: "",
+  state: "",
+  postal_code: "",
+  country: "",
 };
-
-
 
 const mapState = createStructuredSelector({
   total: selectCartTotal,
@@ -37,130 +44,146 @@ const PaymentDetails = () => {
   const history = useHistory();
   const { total, itemCount, cartItems } = useSelector(mapState);
   const dispatch = useDispatch();
-  const [billingAddress, setBillingAddress] = useState({ ...initialAddressState });
-  const [shippingAddress, setShippingAddress] = useState({ ...initialAddressState });
-  const [recipientName, setRecipientName] = useState('');
-  const [nameOnCard, setNameOnCard] = useState('');
-
+  const [billingAddress, setBillingAddress] = useState({
+    ...initialAddressState,
+  });
+  const [shippingAddress, setShippingAddress] = useState({
+    ...initialAddressState,
+  });
+  const [recipientName, setRecipientName] = useState("");
+  const [nameOnCard, setNameOnCard] = useState("");
 
   useEffect(() => {
     if (itemCount < 1) {
-      history.push('/dashboard');
+      history.push("/dashboard");
     }
-
   }, [itemCount]);
 
-  const handleShipping = evt => {
+  const handleShipping = (evt) => {
     const { name, value } = evt.target;
     setShippingAddress({
       ...shippingAddress,
-      [name]: value
+      [name]: value,
     });
   };
 
-  const handleBilling = evt => {
+  const handleBilling = (evt) => {
     const { name, value } = evt.target;
     setBillingAddress({
       ...billingAddress,
-      [name]: value
+      [name]: value,
     });
   };
 
-
-  const handleFormSubmit = async evt => {
+  const handleFormSubmit = async (evt) => {
     evt.preventDefault();
-    const cardElement = elements.getElement('card');
+    const cardElement = elements.getElement("card");
 
     if (
-      !shippingAddress.line1 || !shippingAddress.city ||
-      !shippingAddress.state || !shippingAddress.postal_code ||
-      !shippingAddress.country || !billingAddress.line1 ||
-      !billingAddress.city || !billingAddress.state ||
-      !billingAddress.postal_code || !billingAddress.country ||
-      !recipientName || !nameOnCard
+      !shippingAddress.line1 ||
+      !shippingAddress.city ||
+      !shippingAddress.state ||
+      !shippingAddress.postal_code ||
+      !shippingAddress.country ||
+      !billingAddress.line1 ||
+      !billingAddress.city ||
+      !billingAddress.state ||
+      !billingAddress.postal_code ||
+      !billingAddress.country ||
+      !nameOnCard
     ) {
+      console.log(shippingAddress.line1);
+      console.log(shippingAddress.city);
+      console.log(shippingAddress.state);
+      console.log(shippingAddress.postal_code);
+      console.log(shippingAddress.country);
+      console.log(billingAddress.line1);
+      console.log(billingAddress.city);
+      console.log(billingAddress.state);
+      console.log(billingAddress.postal_code);
+      console.log(billingAddress.country);
+      console.log(recipientName);
+      console.log(nameOnCard);
+
       return;
     }
 
-    apiInstance.post('/payments/create', {
-      amount: total * 100,
-      shipping: {
-        name: recipientName,
-        address: {
-          ...shippingAddress
-        }
-      }
-    }).then(({ data: clientSecret }) => {
-
-      stripe.createPaymentMethod({
-        type: 'card',
-        card: cardElement,
-        billing_details: {
-          name: nameOnCard,
+    apiInstance
+      .post("/payments/create", {
+        amount: total * 100,
+        shipping: {
+          name: recipientName,
           address: {
-            ...billingAddress
-          }
-        }
-      }).then(({ paymentMethod }) => {
-
-        stripe.confirmCardPayment(clientSecret, {
-          payment_method: paymentMethod.id
-        })
-        .then(({ paymentIntent }) => {
-
-          const configOrder = {
-            orderTotal: total,
-            orderItems: cartItems.map(item => {
-              const { documentID, productThumbnail, productName,
-                productPrice, quantity } = item;
-
-              return {
-                documentID,
-                productThumbnail,
-                productName,
-                productPrice,
-                quantity
-              };
-            })
-          }
-
-          dispatch(
-            saveOrderHistory(configOrder)
-          );
-        });
-
+            ...shippingAddress,
+          },
+        },
       })
+      .then(({ data: clientSecret }) => {
+        stripe
+          .createPaymentMethod({
+            type: "card",
+            card: cardElement,
+            billing_details: {
+              name: nameOnCard,
+              address: {
+                ...billingAddress,
+              },
+            },
+          })
+          .then(({ paymentMethod }) => {
+            stripe
+              .confirmCardPayment(clientSecret, {
+                payment_method: paymentMethod.id,
+              })
+              .then(({ paymentIntent }) => {
+                const configOrder = {
+                  orderTotal: total,
+                  orderItems: cartItems.map((item) => {
+                    const {
+                      documentID,
+                      productThumbnail,
+                      productName,
+                      productPrice,
+                      quantity,
+                    } = item;
 
+                    return {
+                      documentID,
+                      productThumbnail,
+                      productName,
+                      productPrice,
+                      quantity,
+                    };
+                  }),
+                };
 
-    });
-
+                dispatch(saveOrderHistory(configOrder));
+              });
+          });
+      });
   };
 
   const configCardElement = {
-    iconStyle: 'solid',
+    iconStyle: "solid",
     style: {
       base: {
-        fontSize: '16px'
-      }
+        fontSize: "16px",
+      },
     },
-    hidePostalCode: true
+    hidePostalCode: true,
   };
-
 
   return (
     <div className="paymentDetails">
       <form onSubmit={handleFormSubmit}>
-
         <div className="group">
-          <h2>
-            Shipping Address
-          </h2>
+          <h2>Shipping Address</h2>
 
           <FormInput
             required
             placeholder="Line 1"
             name="line1"
-            handleChange={evt => handleShipping(evt)}
+            handleChange={(evt) => handleShipping(evt)}
             value={shippingAddress.line1}
             type="text"
           />
@@ -168,7 +191,7 @@ const PaymentDetails = () => {
           <FormInput
             placeholder="Line 2"
             name="line2"
-            handleChange={evt => handleShipping(evt)}
+            handleChange={(evt) => handleShipping(evt)}
             value={shippingAddress.line2}
             type="text"
           />
@@ -177,7 +200,7 @@ const PaymentDetails = () => {
             required
             placeholder="City"
             name="city"
-            handleChange={evt => handleShipping(evt)}
+            handleChange={(evt) => handleShipping(evt)}
             value={shippingAddress.city}
             type="text"
           />
@@ -186,7 +209,7 @@ const PaymentDetails = () => {
             required
             placeholder="State"
             name="state"
-            handleChange={evt => handleShipping(evt)}
+            handleChange={(evt) => handleShipping(evt)}
             value={shippingAddress.state}
             type="text"
           />
@@ -195,7 +218,7 @@ const PaymentDetails = () => {
             required
             placeholder="Postal Code"
             name="postal_code"
-            handleChange={evt => handleShipping(evt)}
+            handleChange={(evt) => handleShipping(evt)}
             value={shippingAddress.postal_code}
             type="number"
           />
@@ -203,29 +226,28 @@ const PaymentDetails = () => {
           <div className="formRow checkoutInput">
             <CountryDropdown
               required
-              onChange={val => handleShipping({
-                target: {
-                  name: 'country',
-                  value: val
-                }
-              })}
+              onChange={(val) =>
+                handleShipping({
+                  target: {
+                    name: "country",
+                    value: val,
+                  },
+                })
+              }
               value={shippingAddress.country}
               valueType="short"
             />
           </div>
-        
         </div>
 
         <div className="group">
-          <h2>
-            Billing Address
-          </h2>
+          <h2>Billing Address</h2>
 
           <FormInput
             required
             placeholder="Name on Card"
             name="nameOnCard"
-            handleChange={evt => setNameOnCard(evt.target.value)}
+            handleChange={(evt) => setNameOnCard(evt.target.value)}
             value={nameOnCard}
             type="text"
           />
@@ -234,7 +256,7 @@ const PaymentDetails = () => {
             required
             placeholder="Line 1"
             name="line1"
-            handleChange={evt => handleBilling(evt)}
+            handleChange={(evt) => handleBilling(evt)}
             value={billingAddress.line1}
             type="text"
           />
@@ -242,7 +264,7 @@ const PaymentDetails = () => {
           <FormInput
             placeholder="Line 2"
             name="line2"
-            handleChange={evt => handleBilling(evt)}
+            handleChange={(evt) => handleBilling(evt)}
             value={billingAddress.line2}
             type="text"
           />
@@ -251,7 +273,7 @@ const PaymentDetails = () => {
             required
             placeholder="City"
             name="city"
-            handleChange={evt => handleBilling(evt)}
+            handleChange={(evt) => handleBilling(evt)}
             value={billingAddress.city}
             type="text"
           />
@@ -260,7 +282,7 @@ const PaymentDetails = () => {
             required
             placeholder="State"
             name="state"
-            handleChange={evt => handleBilling(evt)}
+            handleChange={(evt) => handleBilling(evt)}
             value={billingAddress.state}
             type="text"
           />
@@ -269,63 +291,64 @@ const PaymentDetails = () => {
             required
             placeholder="Postal Code"
             name="postal_code"
-            handleChange={evt => handleBilling(evt)}
+            handleChange={(evt) => handleBilling(evt)}
             value={billingAddress.postal_code}
-            type="number" 
-            
+            type="number"
           />
 
           <div className="formRow checkoutInput">
             <CountryDropdown
               required
-              onChange={val => handleBilling({
-                target: {
-                  name: 'country',
-                  value: val
-                }
-              })}
+              onChange={(val) =>
+                handleBilling({
+                  target: {
+                    name: "country",
+                    value: val,
+                  },
+                })
+              }
               value={billingAddress.country}
               valueType="short"
             />
           </div>
-
         </div>
 
         <div className="group">
-          <h2>
-            Card Details
-          </h2>
+          <h2>Card Details</h2>
 
-          <p>
-            Accepted Card
-          </p>
+          <p>Accepted Card</p>
           <div class="icon-container">
-          <span className="visa"><FontAwesomeIcon icon={faCcVisa} /></span>
-          <span className="amex"><FontAwesomeIcon icon={faCcAmex} /></span>
-          <span className="master"><FontAwesomeIcon icon={faCcMastercard} /></span>
-          <span className="discover"><FontAwesomeIcon icon={faCcDiscover}/></span>
-            </div>
+            <span className="visa">
+              <FontAwesomeIcon icon={faCcVisa} />
+            </span>
+            <span className="amex">
+              <FontAwesomeIcon icon={faCcAmex} />
+            </span>
+            <span className="master">
+              <FontAwesomeIcon icon={faCcMastercard} />
+            </span>
+            <span className="discover">
+              <FontAwesomeIcon icon={faCcDiscover} />
+            </span>
+          </div>
 
-          <CardElement
-            options={configCardElement}
-          />
+          <CardElement options={configCardElement} />
         </div>
 
         <div>
-        <Button class="backBtn" onClick={()=> history.goBack()}>
-          Back to Cart
-        </Button>
-        
-        <span>
-        <Button className="payBtn" type="submit">
-          Pay Now
-        </Button>
-        </span>
-        </div>
+          <Button class="backBtn" onClick={() => history.goBack()}>
+            Back to Cart
+          </Button>
 
+          <span>
+            <Button className="payBtn" htmlType="submit">
+              Pay Now
+            </Button>
+          </span>
+        </div>
       </form>
     </div>
   );
-}
+};
 
 export default PaymentDetails;
--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,91 +1,88 @@
-import React, {useEffect} from 'react';
-import {useParams,useHistory} from 'react-router-dom';
-import {useDispatch,useSelector} from 'react-redux';
-import { fetchProductStart, setProduct } from '../../redux/Products/products.actions';
-import { addProduct } from '../../redux/Cart/cart.actions';
-import Button from '../forms/Button';
-import './styles.scss'
-import productsTypes from '../../redux/Products/products.types';
-import {ShoppingCartOutlined, HeartOutlined} from '@ant-design/icons';
+import React, { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchProductStart,
+  setProduct,
+} from "../../redux/Products/products.actions";
+import { addProduct } from "../../redux/Cart/cart.actions";
+import Button from "../forms/Button";
+import "./styles.scss";
+import productsTypes from "../../redux/Products/products.types";
+import { ShoppingCartOutlined, HeartOutlined } from "@ant-design/icons";
+import { Image } from "antd";
+import BounceLoader from "react-spinners/BounceLoader";
 
-const mapState = state => ({
-    product: state.productsData.product
+const mapState = ({ productsData }) => ({
+  product: productsData.product,
+  loading: productsData.loading,
 });
 
+const ProductCard = ({}) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { productID } = useParams();
+  const [loading, setLoading] = useState(true);
+  const { product, loading: loading1 } = useSelector(mapState);
 
-const ProductCard = ({ }) => {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const {productID} = useParams();
-    const {product} = useSelector(mapState);
+  const { allImageURL, productName, productPrice, productDesc, documentID } =
+    product;
 
-    const {
-        productThumbnail,
-        productName,
-        productPrice,
-        productDesc,
-        documentID
-    } = product;
+  useEffect(() => {
+    dispatch(fetchProductStart(productID));
 
-    useEffect (() => {
-        dispatch(
-            fetchProductStart(productID)
-        )
+    console.log(allImageURL);
+  }, []);
 
-        return() => {
-            dispatch(
-                setProduct({})
-            )
-        }
+  useEffect(() => {
+    setLoading(loading1);
+  }, [loading1]);
+  const handleAddToCart = (product) => {
+    if (!product) return;
+    dispatch(addProduct(product));
+    history.push("/cart");
+  };
 
-    },[]);
+  const configAddToCartBtn = {
+    type: "button",
+  };
 
-    const handleAddToCart= (product) =>{
-        if (!product) return;
-        dispatch(
-            addProduct(product)
-        );
-        history.push('/cart');
-    }
+  return loading ? (
+    <div className="loadingDiv">
+      <BounceLoader color={"black"} loading={loading} size={100} />
+    </div>
+  ) : (
+    <div className="productCard">
+      <div className="hero">
+        <Image src={allImageURL} />
+      </div>
+      <div className="productDetails">
+        <h1>{productName}</h1>
 
-    const configAddToCartBtn = {
-        type: 'button'
-    }
+        <h2>RM{productPrice}</h2>
 
-    return (
-        <div className="productCard">
-            <div className="hero">
-                <img src={productThumbnail}/>
-            </div>
-            <div className="productDetails">
-                
-                    <h1>
-                        {productName}
-                    </h1>
-
-                    <h2>
-                        RM{productPrice}
-                    </h2>
-                
-                <div className="prodDetails">
-                    <p>PRODUCT DETAILS</p>
-                    <span className="productDesc"
-                dangerouslySetInnerHTML={{__html:productDesc}}/>
-                </div>      
-                
-                <div className="addToCart">
-                        <Button className="addCartBtn" {...configAddToCartBtn} onClick={() => handleAddToCart(product)}>
-                            <ShoppingCartOutlined /> &nbsp;&nbsp; Add to Cart
-                        </Button>
-
-                        <Button className="addWishlistBtn">
-                            {/*<HeartOutlined />*/}
-                        </Button>
-                </div>
-                
-            </div>
+        <div className="prodDetails">
+          <p>PRODUCT DETAILS</p>
+          <span
+            className="productDesc"
+            dangerouslySetInnerHTML={{ __html: productDesc }}
+          />
         </div>
-    );
+
+        <div className="addToCart">
+          <Button
+            className="addCartBtn"
+            {...configAddToCartBtn}
+            onClick={() => handleAddToCart(product)}
+          >
+            <ShoppingCartOutlined /> &nbsp;&nbsp; Add to Cart
+          </Button>
+
+          <Button className="addWishlistBtn">{/*<HeartOutlined />*/}</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
